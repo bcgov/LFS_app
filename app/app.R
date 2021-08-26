f2707a63ac80ed09dce26c3062b93ae961f548fa
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2021-08-26 13:42:23 -0700

    comments

--- app/app.R
+++ app/app.R
@@ -26,20 +26,13 @@ ui <- function(req) {
               column(
               width = 12,
               style = "background-color:#003366; border-bottom:2px solid #fcba19; position:fixed; z-index:10000",
-                 tags$header(class="header", style="padding:0 0px 0 0px; display:flex; height:80px;
-                 width:100%;",
-                             # tags$div(class="banner", style="width:100%; display:flex; flex-direction: row; justify-content:center; margin: 0 10px 0 10px",
-                             #          a(href="https://www2.gov.bc.ca/gov/content/data/about-data-management/bc-stats",
-                             #            img(src = "bcstats_logo_rev.png", title = "BC Stats", height = "80px", alt = "British Columbia - BC Stats"),
-                             #            onclick="gtag"),
-                             #          h1("British Columbia - Labour Market Statistics", style="font-weight:400; color:white; margin: 5px 5px 0 18px;"),
-                             #          tags$div(style = "margin-left:auto; margin-right:0;",linkModUI('links'))
-                             # )
+                 tags$header(class="header", style="padding:0 0px 0 0px; display:flex; height:80px; width:100%;",
                    tags$div(class="banner", style="width:100%; display:flex; justify-content:flex-start; align-items:center; margin: 0 10px 0 10px",
                      a(href="https://www2.gov.bc.ca/gov/content/data/about-data-management/bc-stats",
                        img(src = "bcstats_logo_rev.png", title = "BC Stats", height = "80px", alt = "British Columbia - BC Stats"),
                        onclick="gtag"),
                      h1("British Columbia - Labour Market Statistics", style="font-weight:400; color:white; margin: 5px 5px 0 18px;"),
+                     ## MODULE CODE
                      tags$div(style = "margin-left:auto; margin-right:0;",linkModUI('links'))
                    )
                  )
@@ -174,6 +167,7 @@ ui <- function(req) {
 ## define server logic ----
 server <- function(input, output, session) {
   
+  ## MODULE CODE
   linkModServer('links')
   
   ## Tab 0: Highlights ----
@@ -415,7 +409,7 @@ server <- function(input, output, session) {
       DT::datatable(data, 
                     options = list(#dom = "lpt",info = FALSE, paging = TRUE, pagingType = "simple", 
                                    info = FALSE, paging = FALSE,
-                                   searching = FALSE, scrollX = TRUE, scrollY = "350px",
+                                   searching = FALSE, scrollX = TRUE, scrollY = "275px",
                                    columnDefs = list(list(className = 'dt-center', targets = "_all")),
                                    language = list(zeroRecords = zero_data_note)),
                     rownames = FALSE,
@@ -492,13 +486,6 @@ server <- function(input, output, session) {
       )
       
     }
-     
-
-    
-    
-
-    
-    
     
   })
   
