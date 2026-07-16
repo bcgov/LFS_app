62a418f0f041d062388366ff37357302c921ad30
Yurchak <Stephanie.Yurchak@gov.bc.ca>
2026-07-16 16:07:23 -0700

    replace header with bcsHeaderUI

--- app/app.R
+++ app/app.R
@@ -37,24 +37,15 @@ ui <- function(req) {
             
             fluidRow(
               ## Header column ----
-              column(
-              width = 12,
-              style = "background-color:#003366; border-bottom:2px solid #fcba19; position:fixed; z-index:10000",
-                 tags$header(class="header", style="padding:0 0px 0 0px; display:flex; height:80px; width:100%;",
-                   tags$div(class="banner", style="width:100%; display:flex; justify-content:flex-start; align-items:center; margin: 0 10px 0 10px",
-                     a(href="https://www2.gov.bc.ca/gov/content/data/about-data-management/bc-stats",
-                       img(src = "bcstats_logo_rev.png", title = "BC Stats", height = "80px", alt = "British Columbia - BC Stats"),
-                       onclick="gtag"),
-                     h1("Labour Market Statistics for British Columbia", style="font-weight:400; color:white; margin: 5px 5px 0 18px;"),
-                     ## MODULE CODE
-                     tags$div(style = "margin-left:auto; margin-right:0;",linkModUI('links'))
-                   )
-                 )
-              ),  ## end of column
+              bcsHeaderUI(
+                id = "header",
+                appname = "Labour Market Statistics for British Columbia",
+                mobilename = "Labour Market",
+                github = "https://github.com/bcgov/LFS_app"
+              ),
     ## Main body column ----
     ## Make changes to this column
     column(width = 12,
-           style = "margin-top:100px",
            
            ## Tabset start ----  
            navset_tab(id = "tabs",
@@ -248,8 +239,8 @@ ui <- function(req) {
 ## define server logic ----
 server <- function(input, output, session) {
   
-  ## MODULE CODE
-  linkModServer('links')
+  bcsapps::bcsHeaderServer(id = 'header', links = TRUE)
+  bcsapps::bcsFooterServer(id = 'footer')
   
   ## Tab 0: Highlights ----
   
