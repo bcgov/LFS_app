f4bf98805ab6927a0f5807c0b530f2600917cdb9
Yurchak <Stephanie.Yurchak@gov.bc.ca>
2026-08-14 14:28:27 -0700

    move date calculations

--- app/R/startup_highlights_indicators.R
+++ app/R/startup_highlights_indicators.R
@@ -205,6 +205,21 @@ lf_characteristics <- semi_join(
   arrange(label) %>%
   select(group, label_order, label, everything())
 
+## Dates ----
+latest_update_data <- get_cansim_vector_for_latest_periods(vectors = "v2064700", periods = 1)
+release_date <- latest_update_data %>% pull(releaseTime)
+curr_date <- latest_update_data  %>% pull(Date) %>% max()
+prev_month <- curr_date - months(1)
+prev_year <- curr_date - years(1)
+prev_year_jan <- paste0(year(curr_date - years(1)),"-01-01") %>% ymd()
+curr_year_jan <- paste0(year(curr_date), "-01-01") %>% ymd()
+monthly_start_month <- paste0(year(curr_date - years(2)),"-01-01") %>% ymd()
+annual_start_year <- paste0(year(curr_date - years(11)),"-01-01") %>% ymd()
+annual_display_year <- paste0(year(curr_date - years(10)), "01-01") %>% ymd()
+
+formatted_date <- paste(month(curr_date, label = TRUE, abbr = FALSE), year(curr_date))
+last_updated_date <- ymd_hm(release_date) %>% format(format = "%B %e, %Y")
+
 }
 
 
